6fb1c1b21b6943963e5fa6573e1d1f2db62dd9a9
sidmidccompany <sidmidccompany@gmail.com>
2025-05-26 11:24:15 +0530

    Update __manifest__.py

--- hr_zk_attendance/__manifest__.py
+++ hr_zk_attendance/__manifest__.py
@@ -1,18 +1,49 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2025-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Bhagyadev KP (odoo@cybrosys.com)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+################################################################################
 {
     'name': 'Biometric Device Integration',
     'version': '18.0.1.0.0',
     'category': 'Human Resources',
     'summary': "Integrating Biometric Device (Model: ZKteco uFace 202) With HR"
                "Attendance (Face + Thumb)",
-    'description': "This module integrates Odoo with the biometric"
-                   "device(Model: ZKteco uFace 202),odoo18,odoo,hr,attendance",
+    'description': """
+This module integrates Odoo with the biometric device (Model: ZKteco uFace 202).
+Features:
+- Connect to ZKTeco devices over network
+- Download attendance logs
+- Synchronize employee data
+- Manage multiple devices
+- Automatic attendance import
+
+Note: Requires pyzk library and network access to the device.
+    """,
     'author': 'Cybrosys Techno Solutions',
     'company': 'Cybrosys Techno Solutions',
     'maintainer': 'Cybrosys Techno Solutions',
     'website': "https://www.cybrosys.com",
     'depends': ['base_setup', 'hr_attendance'],
     'external_dependencies': {
-        'python': ['pyzk'], },
+        'python': ['pyzk', 'pytz'],
+    },
     'data': [
         'security/ir.model.access.csv',
         'views/biometric_device_details_views.xml',
@@ -25,4 +56,5 @@
     'installable': True,
     'auto_install': False,
     'application': False,
+    'pre_init_hook': 'pre_init_check',
 }
