08adfd24ab9d589f98e050a7a480c3a224f9dfd9
sidmidccompany <sidmidccompany@gmail.com>
2025-05-26 11:24:43 +0530

    Update __init__.py

--- hr_zk_attendance/__init__.py
+++ hr_zk_attendance/__init__.py
@@ -1,1 +1,35 @@
+
+
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2025-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Bhagyadev KP (odoo@cybrosys.com)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+################################################################################
 from . import models
+
+def pre_init_check(cr):
+    """Verify that required dependencies are installed before installing the module"""
+    from odoo.exceptions import UserError
+    try:
+        import zk
+        import pytz
+    except ImportError as e:
+        raise UserError(f'Missing required Python library: {str(e)}. '
+                       f'Please make sure to add it to requirements.txt '
+                       f'and install it on your Odoo.sh project.')
